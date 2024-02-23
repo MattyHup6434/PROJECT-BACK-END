4df2a64628c880f0ff07c131167185a7303857ba
MattyHup6434 <122677300+MattyHup6434@users.noreply.github.com>
2024-02-23 21:21:42 +0700

    create

--- src/controllers/admin-controler.js
+++ src/controllers/admin-controler.js
@@ -3,7 +3,7 @@ const db = require("../models/db");
 exports.createRestaurant = async (req, res, next) => {
   const { name, address, phone, type } = req.body;
   try {
-    // ตรวจสอบความถูกต้องของข้อมูล
+    
     if (!(name && address && phone && type)) {
       return next(new Error("กรุณากรอกข้อมูลให้ครบทุกช่อง"));
     }
